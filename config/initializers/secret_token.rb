ee903538ab21f95bc9f7cb788c2f5a3697f8183a
David Clarke <david.clarke@rea-group.com>
2012-09-10 13:57:20 +1000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamoDbHackday::Application.config.secret_token = 'b2172a5fa1696d2d2842720eccba7b3e4d1c3ded91ec034d6cfb20ca81ab99aa3f2d491702e90c93f6e73e1532844fb6f5e5a182b64e9f1d7bc99dcf487ea1eb'
